3cfeac68b517c1c3c7b3bbb25fcc6ce738832a1b
unknown <ocelot269@gmail.com>
2019-01-19 17:27:35 +0100

    Cambios menores

---
+++ Codewars/kata8/Parse_nice_int/CharProblem/src/charproblem/CharProblem.java
@@ -0,0 +1,18 @@
+
+package charproblem;
+
+
+public class CharProblem {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+  public static int howOld(final String herOld) {
+    int edad = Integer.parseInt("" + herOld.charAt(0));
+    return edad;
+  
+  }
+}
+
