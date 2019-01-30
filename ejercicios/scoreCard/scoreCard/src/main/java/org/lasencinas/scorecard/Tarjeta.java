f2a981122652ba9dcb183bb23e193e997ef42e14
unknown <ocelot269@gmail.com>
2019-01-30 17:34:19 +0100

    Pasando el spare

--- ejercicios/scoreCard/scoreCard/src/main/java/org/lasencinas/scorecard/Tarjeta.java
+++ ejercicios/scoreCard/scoreCard/src/main/java/org/lasencinas/scorecard/Tarjeta.java
@@ -4,13 +4,58 @@ package org.lasencinas.scorecard;
 
 public class Tarjeta {
     
+    private final int STRIKE = 10;
+    private final int CERO = 0;
+    private String pines="-123456789X/";
+    private String tarjeta =null ;
+    private int puntuacionTotal = 0;
+    private final int SPARE =10;
     
-    public Tarjeta {
+    public Tarjeta() {
     
     }
+    public Tarjeta(String tarjeta){
+        this.tarjeta = tarjeta;
+    }
+    public String getTarjetaPuntuacion(){
+        return this.tarjeta;
+    }
+    public int getStrike(){
+        return STRIKE;
+    }
+    public int computarStrike(char strike){
+        if (strike == 'X'){
+           return STRIKE;   
+        }
+        else{
+            return CERO;
+        }
+        
+    }
+    public int computarSpare(char spare){
+        if (spare == ('/')){
+            return SPARE;
+        }
+        else {
+            return CERO;
+        }
+    }
+    public int computarPins(char pin){
+        return this.pines.indexOf(pin);
+    }
     
-    
-    
+    public int computarTarjetaVeinteBolas(){
+        for (int i=0; i <getTarjetaPuntuacion().length(); i++)
+            if (getTarjetaPuntuacion().charAt(i)== '/'){
+                if (i < 19 ) {
+                    puntuacionTotal+= -getTarjetaPuntuacion().charAt(i - 1) + 10 + getTarjetaPuntuacion().charAt(i + 1); 
+                }
+            }
+            else {
+                puntuacionTotal+= computarPins(getTarjetaPuntuacion().charAt(i));
+            } 
+        return puntuacionTotal;
+    }
     
     
 }
--- ejercicios/scoreCard/scoreCard/src/test/java/org/lasencinas/scorecard/TarjetaIT.java
+++ ejercicios/scoreCard/scoreCard/src/test/java/org/lasencinas/scorecard/TarjetaIT.java
@@ -1,24 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package org.lasencinas.scorecard;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
 
-/**
- *
- * @author ozeh
- */
 public class TarjetaIT {
+    private Tarjeta tarjeta = null;
     
-    public TarjetaIT() {
+        @Before
+        public void init(){
+            tarjeta = new Tarjeta();
     }
-
+    
     @Test
-    public void testSomeMethod() {
+    public void testcomputarStrike() {
+        assertEquals(10, tarjeta.computarStrike('X'));
+        assertEquals(0, tarjeta.computarStrike('9'));
     }
-    
+    @Test
+    public void computarPinsTest(){
+        for (int i = 1; i<= 9; i++)
+        assertEquals(9,tarjeta.computarPins('9'));
+        assertEquals(0,tarjeta.computarPins('-'));
+    }
+    @Test
+    public void computarTarjetaVeinteBolasTest(){
+         tarjeta = new Tarjeta("12345123451234512345");
+        assertEquals(60,tarjeta.computarTarjetaVeinteBolas());
+    }
+    @Test
+    public void computarTarjetaVeinteBolasTest1(){
+        Tarjeta tarjeta= new Tarjeta("11111111111111111111");
+        assertEquals(20,tarjeta.computarTarjetaVeinteBolas());
+    }
+    @Test
+    public void computarTarjetaVeinteBolasTest2(){
+        Tarjeta tarjeta= new Tarjeta("9-9-9-9-9-9-9-9-9-9-");
+        assertEquals(90,tarjeta.computarTarjetaVeinteBolas());
+    }
+    @Test
+    public void computarTarjetaVeinteBolasTest3(){
+        Tarjeta tarjeta= new Tarjeta("5/5/5/5/5/5/5/5/5/5-5");
+        assertEquals(150,tarjeta.computarTarjetaVeinteBolas());
+    }
 }
