146797d9ffdc073ae3d19d3ccdc191e0e796d762
unknown <ocelot269@gmail.com>
2019-02-10 12:42:42 +0100

    Creando variables de clase conductores y creado el constructor

--- cotxox/cotxox/src/main/java/org/lasencinas/cotxox/conductores/Conductor.java
+++ cotxox/cotxox/src/main/java/org/lasencinas/cotxox/conductores/Conductor.java
@@ -1,14 +1,61 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package org.lasencinas.cotxox.conductores;
 
-/**
- *
- * @author ozeh
- */
+import java.util.ArrayList;
+
+
 public class Conductor {
     
+    
+//------------------------Inicializando variables------------------------------------------------//    
+  
+   private String nombre;
+   private String modelo;
+   private String matricula;
+   private double valoracionesMedia=0d;
+   private ArrayList<Double>valoraciones= new ArrayList<>();
+   private boolean ocupado=false;
+   
+   
+//------------------------------Fin variables------------------------------------------//   
+
+   
+//-------------------------------Iniciando Constructor---------------------------------//
+
+   
+   public Conductor(){
+       
+   }
+   
+   
+   //--------------------Sobrecarga Constructor-------------------------------//
+   
+
+   public Conductor (String nombre){
+       this.nombre=nombre;
+   }
+   
+   
+   //------------------Fin Sobrecarga Constructor-----------------------------//
+   
+   
+//---------------------------------Fin Inicio Constructor--------------------------------//
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
 }
+
---
+++ cotxox/cotxox/src/test/java/org/lasencinas/cotxox/conductores/ConductorIT.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.lasencinas.cotxox.conductores;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ozeh
+ */
+public class ConductorIT {
+    
+    public ConductorIT() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
