e16ae64dcdb001dc58a65fb127d116e5388cee1c
unknown <ocelot269@gmail.com>
2019-03-12 17:44:22 +0100

    añadido proyecto

--- Herencia/casting/UpDownCasting/src/main/java/org/lasencinas/updowncasting/LateBindingSub.java
+++ Herencia/casting/UpDownCasting/src/main/java/org/lasencinas/updowncasting/LateBindingSub.java
@@ -6,7 +6,6 @@
 package org.lasencinas.updowncasting;
 
 // LateBindingSub.java
-package com.jdojo.inheritance;
 public class LateBindingSub extends LateBindingSuper{
  public void print() {
  System.out.println("Inside LateBindingSub.print()");
