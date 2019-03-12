35b196f91029c1df3c581b558c988ec62c97044f
unknown <ocelot269@gmail.com>
2019-03-12 17:21:42 +0100

    Actualizacion de estructuras de datos

--- Bibliotecas/Bibliotecas/src/main/java/org/lasencinas/bibliotecas/Ejemplar.java
+++ Bibliotecas/Bibliotecas/src/main/java/org/lasencinas/bibliotecas/Ejemplar.java
@@ -1,38 +1,39 @@
-
 package org.lasencinas.bibliotecas;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
-
 public class Ejemplar {
-    
+
     //Variables
-    private Set<Libro> ListaLibros = new HashSet<>();
-    private Set<Revista> ListaRevistas = new HashSet<>();
-    
+    private HashMap<String, Integer> listaLibros = new HashMap<String, Integer>();
+    private HashMap<String, Integer> listaRevistas = new HashMap<String, Integer>();
+
     //Constructor
     public Ejemplar() {
-        
+
+    }
+//getters y setters
+
+    public HashMap<String, Integer> getListaLibros() {
+        return listaLibros;
+    }
+    
+    
+    public void setListaLibros(HashMap<String, Integer> listaLibros) {
+        this.listaLibros = listaLibros;
+    }
+    
+    
+    public HashMap<String, Integer> getListaRevistas() {
+        return listaRevistas;
+    }
+    
+    
+    public void setListaRevistas(HashMap<String, Integer> listaRevistas) {
+        this.listaRevistas = listaRevistas;
     }
 
     
-    public Set<Libro> getListaLibros() {
-        return ListaLibros;
-    }
-
-    
-    public void setListaLibros(Set<Libro> ListaLibros) {
-        this.ListaLibros = ListaLibros;
-    }
-
-    
-    public Set<Revista> getListaRevistas() {
-        return ListaRevistas;
-    }
-
-    
-    public void setListaRevistas(Set<Revista> ListaRevistas) {
-        this.ListaRevistas = ListaRevistas;
-    }
 }
