59dfcd4d8a7a294b2010264b7f20fb9461b1b871
unknown <ocelot269@gmail.com>
2019-04-02 00:10:06 +0200

    Refactorizacion y creado un metodo sobreCargado

--- expresionesRegulares/numeralesRomanos/NumeralesRomanos/src/main/java/org/lasencinas/numeralesromanos/NumerosRomanos.java
+++ expresionesRegulares/numeralesRomanos/NumeralesRomanos/src/main/java/org/lasencinas/numeralesromanos/NumerosRomanos.java
@@ -7,7 +7,8 @@ public class NumerosRomanos {
 
     //variables
     private String numeroRomano;
-    private int NumeroRomanoTraducido;
+    private int numeroRomanoTraducido;
+    private String ExpresionRegular = "^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IV|IX|V?I{0,3})$";
 
     //Constructor
     public NumerosRomanos(String numeroRomanos) {
@@ -24,15 +25,28 @@ public class NumerosRomanos {
     }
 
     public int getNumeroRomanoTraducido() {
-        return this.NumeroRomanoTraducido;
+        return this.numeroRomanoTraducido;
     }
 
     public void setValor(int valor) {
-        this.NumeroRomanoTraducido += valor;
+        this.numeroRomanoTraducido += valor;
+        
     }
+    
+    
+    public String getExpresionRegular() {
+        return ExpresionRegular;
+    }
+    
+    
+    
 
     //Logica
+    
+    
     //Aplicando S.O de SOLID
+    
+    
     public int calcularNumRomanosRestan() {
 
         int sumaNumerosNegativos = 0; //Suma total de los pares que restan
@@ -68,10 +82,11 @@ public class NumerosRomanos {
         }
         return getNumeroRomanoTraducido();
     }
-
+    
+    
     public boolean validadorNumeroRomanos() { //Este metodo te compara el numero romano con tu expresion regular
 
-        Pattern verificador = Pattern.compile("^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IV|IX|V?I{0,3})$"); //Expresion regular para numeros romanos
+        Pattern verificador = Pattern.compile(getExpresionRegular()); //Expresion regular para numeros romanos
         //la expresion comprueba que el numero romano empieze con 0 a 3 "M" 
         //Puede contener "CM" "CD" o no  //"D" la puede estar 1 vez o no estar y la "C" de 0 a 3
         //Puede contener "XC" "XL" o no  //"L" la puede estar 1 vez o no estar y la "X" de 0 a 3
@@ -80,16 +95,32 @@ public class NumerosRomanos {
         return comparador.matches();//Te devuelve true o false segun si la expresion es valida
 
     }
+    
+    
+    public boolean validadorNumeroRomanos(String numeroRomano){
+      Pattern verificador = Pattern.compile(getExpresionRegular()); //Expresion regular para numeros romanos
+        //La expresion regular es : "^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IV|IX|V?I{0,3})$"
+        //la expresion comprueba que el numero romano empieze con 0 a 3 "M" 
+        //Puede contener "CM" "CD" o no  //"D" la puede estar 1 vez o no estar y la "C" de 0 a 3
+        //Puede contener "XC" "XL" o no  //"L" la puede estar 1 vez o no estar y la "X" de 0 a 3
+        //Puede contener "IV" "IX" o no  //"V" la puede estar 1 vez o no estar y la "I" de 0 a 3
+        Matcher comparador = verificador.matcher(numeroRomano); //Compara la expresion con el formato del numero romano
+        
+        return comparador.matches();//Te devuelve true o false segun si la expresion es valida  
+    }
+    
 
-    public int traductorNumerosRomanos() {
+    public int traductorNumerosRomanos() { //Te permite averiguar si el numero romano es valido y si lo es,
+        //sera calculado por este metodo
+                            
         int resultado = 0;
 
-        if (validadorNumeroRomanos()) {
-            resultado = calcularNumRomanosRestan() + calcularNumerosRomanosSuman();
+        if (validadorNumeroRomanos()) { //Comprueba si es valido el numero romano
+             resultado = calcularNumRomanosRestan() + calcularNumerosRomanosSuman();
         } else {
-            System.out.println("Error el numero " + getNumeroRomano() + " no es un numero romano valido");
+            System.out.println("Error el numero " + getNumeroRomano() + " no es un numero romano valido"); 
         }
-        return resultado;
+        return resultado;//Si el numero no es valido devuelve 0
     }
 
 }
